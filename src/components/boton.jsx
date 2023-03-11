a52f27195d43a88c5a837a5162696962d8c7034e
Luis Fernando <93270513+Novato117@users.noreply.github.com>
2023-03-11 17:29:53 -0600

    Update boton.jsx

--- src/components/boton.jsx
+++ src/components/boton.jsx
@@ -6,7 +6,7 @@ function Boton() {
     /* const providerRPC = {
          moonbase: {
              name: 'moonbase-alpha',
-             rpc: 'https://rpc.api.moonbase.moonbeam.network',
+             rpc: '',
              chainId: 1287, // 0x507 in hex,
          },
      }
@@ -20,10 +20,10 @@ function Boton() {
  
      // 2. Create account variables
      const account_from = {
-         privateKey: '32dd19af003a745afa2c6053184e723132e28eda23f0242529c75ef2d982944b',
+         privateKey: ',
      };
      //direccion de envio
-     const addressTo = '0xc3baAe4ae7b920F7FF3D08e342113D5A2A5a089e';
+     const addressTo = '';
      let wallet = new ethers.Wallet(account_from.privateKey, provider);
  
      const [adress, setAdress] = useState();
